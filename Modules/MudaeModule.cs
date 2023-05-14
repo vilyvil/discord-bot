9446028e3ecce3387f44ec41fdbceb3e62be0f2e
fieryliv <49703948+fieryliv@users.noreply.github.com>
2023-05-13 20:13:00 -0600

    Update MudaeModule.cs

--- Modules/MudaeModule.cs
+++ Modules/MudaeModule.cs
@@ -5,10 +5,17 @@ using Discord.WebSocket;
 [Group("mudae")]
 public class MudaeModule : ModuleBase<SocketCommandContext> {
     [Command]
-    public async Task getMudaeResponse([Remainder] string cmdParam) {
-        
+    public async Task getMudaeResponse(string cmdParam, [Remainder] string additionalParameters) {
+        if (!additionalParameters.Equals("")) {
+            await Context.Channel.SendMessageAsync("Sorry, this command only supports informational commands! (infokl, infokeys, etc)."
+                                                    + " Please use the real Mudae bot instead.");
+            return;
+        }
+
+
     }
 
+    // TODO FIGURE OUT WHAT TO DO WITH THIS
     [Command("add")]
     public async Task addMudaeResponse() {
         
