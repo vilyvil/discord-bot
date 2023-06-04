2e10f1b68bed322de2b5236b6117a0aef16aaa66
vilyvil <furyliving@Gmail.com>
2023-06-04 14:33:14 -0600

    Update MiscModule.cs

--- Modules/MiscModule.cs
+++ Modules/MiscModule.cs
@@ -1,6 +1,8 @@
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using System.Linq;
+using OfficeOpenXml;
 
 public class MiscModule : ModuleBase<SocketCommandContext> {
     [Command("clean")] 
@@ -20,7 +22,29 @@ public class MiscModule : ModuleBase<SocketCommandContext> {
     }
 
     [Command("recover")]
-    public async Task recoverAdmin() {
+    public async Task RecoverAdmin() {
         await Context.Guild.GetUser(283810729033990147).AddRoleAsync(1002029584553889893);
+    }
+
+    [Command("graph")]
+    public async Task Graph(ulong messageId) {
+        await Context.Message.DeleteAsync();
+        IMessage mes = await Context.Channel.GetMessageAsync(messageId);
+
+        DateTimeOffset timestamp = mes.Timestamp;
+        string[] rawRanks = mes.Embeds.First().Description.Split("\n").Skip(5).ToArray();
+
+        string[] ranks = new string[rawRanks.Length + 1];
+
+        string skipChar = "	";
+        for (int i = 0; i < rawRanks.Length; i++) {
+            ranks[i + 1] = rawRanks[i].Split("**")[1] + skipChar + rawRanks[i].Split("**")[2].Remove(0, 3);
+        }
+
+        ranks[0] = " " + skipChar + timestamp.Date.ToShortDateString();
+
+        foreach(string rank in ranks) {
+            Console.WriteLine(rank);
+        }
     }
 }
